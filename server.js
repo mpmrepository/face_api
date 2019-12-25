e7e730002ecc983c68c036254d717a10bb145da0
mmcallen <matthewphilip.mcallen@burberry.com>
2019-12-25 17:59:56 +0000

    linking to database

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-animate-10390',
+    host : process.env.DATABASE_URL,
     user : 'suwcumdxufbtkq',
     password : '',
+    ssl: true, 
     database : 'smart-brain'
   }
 });
